5bf877f9d10e88cbf005dfc0744c188025d220fa
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2022-12-21 19:07:43 +0000

    Added experience

--- src/App.js
+++ src/App.js
@@ -19,14 +19,52 @@ function App() {
     <div className='layout'>
       <section className='card neumorphism-card-big'>
         <h1 className='title'>Education & Experience</h1>
-        <p className='subtitle'>
+        {/* <p className='subtitle'>
           For more information, have a look at my <a className='highlight-link' href="public/resume.txt" download="Resume.txt">resume</a>
-        </p>
-        <div>
-          <ul>
+        </p> */}
+        <div className='timeline-wrapper'>
+          <ul className='timeline'>
             <li>
-              <div>
-                
+              <div className='direction-r'>
+                <div className='flag-wrapper'>
+                  <span className='hexagon'></span>
+                  <span className='flag neumorphism-button' data-aos="zoom-in-left">
+                    <a href="https://tcs.com/" target="_blank" rel="noreferrer">
+                      <span>Tata Consultancy Services</span>
+                    </a>
+                  </span>
+                  <span className='time-wrapper' data-aos="zoom-in-left">
+                    <span className='time neumorphism-card'>Aug 2021 - Present</span>
+                  </span>
+                </div>
+                {/* <div className='neumorphism-card-mid'> */}
+                  <div className="desc neumorphism-card" data-aos="zoom-in-left">
+                    Full Stack Developer
+                      <div className="tag-wrapper">
+                          <span className="tag neumorphism-card">ReactJS</span>
+                          <span className="tag neumorphism-card">Angular</span>
+                          <span className="tag neumorphism-card">NodeJS</span>
+                          <span className="tag neumorphism-card">ExpressJS</span>
+                          <span className="tag neumorphism-card">Jest</span>
+                          <span className="tag neumorphism-card">Spring boot</span>
+                          <span className="tag neumorphism-card">Java 8</span>
+                          <span className="tag neumorphism-card">Junit</span>
+                          <span className="tag neumorphism-card">Mockito</span>
+                          <span className="tag neumorphism-card">Cucumber</span>
+                          <span className="tag neumorphism-card">AWS</span>
+                          <span className="tag neumorphism-card">Jira</span>
+                          <span className="tag neumorphism-card">Bitbucket</span>
+                      </div>
+                  </div>
+                  {/* <div className="desc neumorphism-card" data-aos="zoom-in-left">
+                    Tools
+                      <div className="tag-wrapper">
+                          <span className="tag neumorphism-card">VS Code</span>
+                          <span className="tag neumorphism-card">STS</span>
+                          <span className="tag neumorphism-card">POSTMAN</span>
+                      </div>
+                  </div> */}
+                {/* </div> */}
               </div>
             </li>
           </ul>
