50f0082bfa725b9b71321a76a96c3f18a6d3e498
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-01-25 18:06:41 +0000

    Use Footer component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Portfolio } from './component/Projects';
 import { Experience } from './component/Experience';
 import { Skills } from './component/Skills';
 import { Contact } from './component/Contact';
+import { Footer } from './component/Footer';
 
 function App() {
   return (
@@ -26,20 +27,7 @@ function App() {
         <Skills />
         <Contact />
       </div>
-      <footer className="footer">
-        <p>
-          &copy; 2023 ClassHacker
-        </p>
-        <p>
-          <a className="highlight-link" href="https://github.com/ClassHacker/classhacker.github.io" target="_blank" rel="noreferrer">
-            Published
-          </a>
-          &#160;with&#160;
-          <a className="highlight-link" href="https://pages.github.com/" target="_blank" rel="noreferrer">
-            GitHub Pages
-          </a>
-        </p>
-      </footer>
+      <Footer/>
     </>
   );
 }
