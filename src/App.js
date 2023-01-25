ca62f72bc50b99523ed985fcf19ef2e999659d20
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-01-25 18:01:35 +0000

    use className instead of just class

--- src/App.js
+++ src/App.js
@@ -26,16 +26,16 @@ function App() {
         <Skills />
         <Contact />
       </div>
-      <footer class="footer">
+      <footer className="footer">
         <p>
           &copy; 2023 ClassHacker
         </p>
         <p>
-          <a class="highlight-link" href="https://github.com/ClassHacker/classhacker.github.io" target="_blank" rel="noreferrer">
+          <a className="highlight-link" href="https://github.com/ClassHacker/classhacker.github.io" target="_blank" rel="noreferrer">
             Published
           </a>
           &#160;with&#160;
-          <a class="highlight-link" href="https://pages.github.com/" target="_blank" rel="noreferrer">
+          <a className="highlight-link" href="https://pages.github.com/" target="_blank" rel="noreferrer">
             GitHub Pages
           </a>
         </p>
