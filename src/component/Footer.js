154ef69078c5c67260884189a6601fa57db9dfb4
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-01-25 18:04:29 +0000

    Created Footer.js

---
+++ src/component/Footer.js
@@ -0,0 +1,19 @@
+
+export function Footer() {
+    return (
+    <footer className="footer">
+      <p>
+        &copy; 2023 ClassHacker
+      </p>
+      <p>
+        <a className="highlight-link" href="https://github.com/ClassHacker/classhacker.github.io" target="_blank" rel="noreferrer">
+          Published
+        </a>
+        &#160;with&#160;
+        <a className="highlight-link" href="https://pages.github.com/" target="_blank" rel="noreferrer">
+          GitHub Pages
+        </a>
+      </p>
+    </footer>
+    )
+}
