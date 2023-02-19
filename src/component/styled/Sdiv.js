e97316578aa71784c016a9897e15737fc457bf08
classhacker <ajaydayma1401@gmail.com>
2023-02-19 13:27:42 +0530

    update style div factory

--- src/component/styled/Sdiv.js
+++ src/component/styled/Sdiv.js
@@ -1,15 +1,12 @@
 import styled, { keyframes } from 'styled-components';
 import { merge, bounce, fadeInDown, fadeIn, zoomIn, flash, flipInX } from 'react-animations';
 
-const fadeInXZoomIn = merge(bounce, fadeIn)
+export const fadeInXZoomIn = merge(bounce, fadeIn)
 
-const Bounce = styled.div.attrs({})` animation: 2s ${keyframes `${bounce}`} infinite `;
-const FlipInX = styled.div.attrs({})` animation: 2s ${keyframes `${flipInX}`} infinite `;
-const FadeIn = styled.div.attrs({})` animation: 2s ${keyframes `${fadeIn}`} `;
-const ZoomIn = styled.div.attrs({})` animation: 2s ${keyframes `${zoomIn}`} `;
-const FadeInDown = styled.div.attrs({})` animation: 2s ${keyframes `${fadeInDown}`} `;
-const FadeInXZoomIn = styled.div.attrs({})` animation: 2s ${keyframes `${fadeInXZoomIn}`} infinite`;
-
+export const Bounce = styled.div.attrs({})` animation: 2s ${keyframes `${bounce}`} infinite `;
+export const FlipInX = styled.div.attrs({})` animation: 2s ${keyframes `${flipInX}`} infinite `;
+export const FadeIn = styled.div.attrs({})` animation: 2s ${keyframes `${fadeIn}`} `;
+export const ZoomIn = styled.div.attrs({})` animation: 2s ${keyframes `${zoomIn}`} `;
+export const FadeInDown = styled.div.attrs({})` animation: 2s ${keyframes `${fadeInDown}`} `;
+export const FadeInXZoomIn = styled.div.attrs({})` animation: 2s ${keyframes `${fadeInXZoomIn}`} infinite`;
 export const Flash = styled.h1.attrs({})` animation: 5s ${keyframes `${flash}`}`;
-
-export default FlipInX;
