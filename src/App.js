0b535ca9c2394365f7bfb34fa879514ebb95648e
classhacker <ajaydayma1401@gmail.com>
2023-02-23 19:04:47 +0530

    update contact card animation

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         <AnimationOnScroll animateIn="animate__bounceInDown" animateOut="animate__bounceOutUp">
           <Skills />
         </AnimationOnScroll>
-        <AnimationOnScroll animateIn="animate__fadeIn" animateOut="animate__fadeOut">
+        <AnimationOnScroll animateIn="animate__fadeInUp" animateOut="animate__fadeOutDown">
           <Contact/>
         </AnimationOnScroll>
       </div>
