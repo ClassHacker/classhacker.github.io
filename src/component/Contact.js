850bf5bdc14e096a95c74351d09928f90461bdc7
classhacker <ajaydayma1401@gmail.com>
2023-04-10 19:05:48 +0530

    lint: error fix

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,11 +1,11 @@
-import { Icons } from "./Icons"
+import { Icons } from './Icons';
 
-export function Contact() {
-    return (
+export function Contact () {
+  return (
         <section className="card neumorphism-card-big">
           <h1 className="title">Contacts</h1>
           <p className="subtitle">Feel free to contact me wherever is comfortable for you</p>
           <Icons type="contact-icons"/>
         </section>
-    )
+  );
 }
