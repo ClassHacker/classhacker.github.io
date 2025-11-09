300af353628bcbba8d227ae91a5df97541c7a5d3
ClassHacker <ajaydayma1401@gmail.com>
2025-11-09 13:51:22 +0530

    Added apexon experience

--- src/component/Experience.js
+++ src/component/Experience.js
@@ -12,6 +12,48 @@ export function Experience () {
         <div className='timeline-wrapper'>
           <ul className='timeline'>
             <li>
+              <div className='direction-l'>
+                <AnimationOnScroll animateIn='animate__fadeIn' animateOut='animate__fadeOut'>
+                  <div className='flag-wrapper'>
+                    <span className='hexagon'></span>
+                    <span className='flag neumorphism-button' data-aos="zoom-in-left">
+                      <a href="https://www.apexon.com/" target="_blank" rel="noreferrer">
+                        <span>Apexon</span>
+                      </a>
+                    </span>
+                    <span className='time-wrapper' data-aos="zoom-in-left">
+                      <span className='time neumorphism-card'>July 2025 - Present</span>
+                    </span>
+                  </div>
+                  <div className='neumorphism-card-mid'>
+                    System Engineer
+                    <div className="desc neumorphism-card" data-aos="zoom-in-left">
+                      Framework & Languages Used
+                        <div className="tag-wrapper">
+                            <span className="tag neumorphism-card">Java 8</span>
+                            <span className="tag neumorphism-card">Spring Framework</span>
+                            <span className="tag neumorphism-card">Spring Boot</span>
+                            <span className="tag neumorphism-card">Camunda Platform</span>
+                            <span className="tag neumorphism-card">Junit</span>
+                            <span className="tag neumorphism-card">Mockito</span>
+                            <span className="tag neumorphism-card">MySQL</span>
+                        </div>
+                    </div>
+                    <div className="desc neumorphism-card" data-aos="zoom-in-left">
+                      IDEs & Tools Used
+                        <div className="tag-wrapper">
+                            <span className="tag neumorphism-card">Intellij IDEA</span>
+                            <span className="tag neumorphism-card">Postman</span>
+                            <span className="tag neumorphism-card">GitLab</span>
+                            <span className="tag neumorphism-card">GitHub Copilot</span>
+                            <span className="tag neumorphism-card">Bitbucket</span>
+                        </div>
+                    </div>
+                  </div>
+                </AnimationOnScroll>
+              </div>
+            </li>
+            <li>
               <div className='direction-r'>
                 <AnimationOnScroll animateIn='animate__fadeIn' animateOut='animate__fadeOut'>
                   <div className='flag-wrapper'>
@@ -22,7 +64,7 @@ export function Experience () {
                       </a>
                     </span>
                     <span className='time-wrapper' data-aos="zoom-in-left">
-                      <span className='time neumorphism-card'>Aug 2021 - Present</span>
+                      <span className='time neumorphism-card'>Aug 2021 - July 2025</span>
                     </span>
                   </div>
                   <div className='neumorphism-card-mid'>
@@ -86,12 +128,9 @@ export function Experience () {
                         <span>Bansal College Of Engineering</span>
                       </a>
                     </span>
-                    {/* <span className='time-wrapper' data-aos="zoom-in-left">
-                      <span className='time neumorphism-card'>July 2017 - July 2021</span>
-                    </span> */}
                   </div>
                   <div className='neumorphism-card-mid'>
-                  Bachelor of Technology
+                    Bachelor of Technology
                     <div className="desc neumorphism-card" data-aos="zoom-in-left">
                       2017-21
                     </div>
@@ -100,36 +139,6 @@ export function Experience () {
                     </div>
                     <div className="desc neumorphism-card" data-aos="zoom-in-left">
                       Computer Science Engineering
-                    </div>
-                  </div>
-                </AnimationOnScroll>
-              </div>
-            </li>
-            <li>
-              <div className='direction-r'>
-                <AnimationOnScroll animateIn='animate__fadeIn' animateOut='animate__fadeOut'>
-                  <div className='flag-wrapper'>
-                    <span className='hexagon'></span>
-                    <span className='flag neumorphism-button' data-aos="zoom-in-left">
-                        <span>Tagore Model HS School</span>
-                    </span>
-                  </div>
-                  <div className='neumorphism-card-mid'>
-                  Class XII (MPBSE)
-                    <div className="desc neumorphism-card" data-aos="zoom-in-left">
-                      77%
-                    </div>
-                    <div className="desc neumorphism-card" data-aos="zoom-in-left">
-                      2016-17
-                    </div>
-                  </div>
-                  <div className='neumorphism-card-mid'>
-                  Class X (MPBSE)
-                    <div className="desc neumorphism-card" data-aos="zoom-in-left">
-                      70%
-                    </div>
-                    <div className="desc neumorphism-card" data-aos="zoom-in-left">
-                      2014-15
                     </div>
                   </div>
                 </AnimationOnScroll>
